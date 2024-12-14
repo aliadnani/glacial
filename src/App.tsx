1f29b8b33217cf2cbe71b1dd2a45deec16fc7e82
Ali <ali.adnani@hotmail.com>
2024-12-15 00:07:42 +0800

    feat: wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,207 @@
 import "./App.css";
 import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
 import { Separator } from "./components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
 import { P } from "./components/ui/typography";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { QRCodeSVG } from "qrcode.react";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
+import { useEffect, useRef, useState } from "react";
+import { useReactToPrint } from "react-to-print";
+import { encode } from "./z85";
+
+const formSchema = z.object({
+  fileName: z.string(),
+  bytesPerQrCode: z.string(),
+  maxQrCodesPerPage: z.number().min(6).max(6),
+  pageSize: z.enum(["A4"]),
+});
+
+function ArchiveForm() {
+  const [file, setFile] = useState<File>();
+  const contentRef = useRef<HTMLDivElement>(null);
+  const reactToPrintFn = useReactToPrint({ contentRef });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      bytesPerQrCode: "32",
+      maxQrCodesPerPage: 6,
+      pageSize: "A4",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        {/* File Input */}
+        <FormField
+          control={form.control}
+          name="fileName"
+          render={({ field }) => (
+            <FormItem className="my-2">
+              <FormLabel>File</FormLabel>
+
+              <FormDescription>
+                Upload a file to encode your data as QR codes.
+              </FormDescription>
+              <FormControl>
+                <Input
+                  type="file"
+                  {...field}
+                  onChange={(e) => {
+                    field.onChange(e);
+                    setFile(e.target.files?.[0] as File);
+                  }}
+                />
+              </FormControl>
+              <FormMessage>
+                {form.formState.errors.fileName?.message?.toString()}
+              </FormMessage>
+            </FormItem>
+          )}
+        />
+
+        {/* Bytes per QR Code */}
+        <FormField
+          control={form.control}
+          name="bytesPerQrCode"
+          render={({ field }) => (
+            <FormItem className="my-2">
+              <FormLabel>Max Bytes per QR Code</FormLabel>
+              <FormDescription>
+                Specify the number of max bytes each QR code should encode.
+              </FormDescription>
+              <Select
+                onValueChange={field.onChange}
+                defaultValue={field.value.toLocaleString()}
+              >
+                <FormControl>
+                  <SelectTrigger className="w-32">
+                    <SelectValue placeholder="Select" defaultValue={"128"} />
+                  </SelectTrigger>
+                  {/* <Input type="number" {...field} /> */}
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value={"32"}>32 Bytes</SelectItem>
+                  <SelectItem value={"128"}>128 Bytes</SelectItem>
+                  <SelectItem value={"512"}>512 Bytes</SelectItem>
+                  <SelectItem value={"2048"}>2048 Bytes</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <FormMessage>
+                {form.formState.errors.bytesPerQrCode?.message}
+              </FormMessage>
+            </FormItem>
+          )}
+        />
+
+        {/* Max QR Codes per Page */}
+        <FormField
+          control={form.control}
+          name="maxQrCodesPerPage"
+          render={({ field }) => (
+            <FormItem className="my-2">
+              <FormLabel>Max QR Codes per Page</FormLabel>
+              <FormDescription>
+                Specify the maximum number of QR codes that can fit on a single
+                page. (WIP - only 6 right now)
+              </FormDescription>
+              <FormControl>
+                <Input className="w-32" type="number" {...field} disabled />
+              </FormControl>
+
+              <FormMessage>
+                {form.formState.errors.maxQrCodesPerPage?.message}
+              </FormMessage>
+            </FormItem>
+          )}
+        />
+
+        {/* Page Size */}
+        <FormField
+          control={form.control}
+          name="pageSize"
+          render={({ field }) => (
+            <FormItem className="my-2">
+              <FormLabel>Page Size</FormLabel>
+              <FormDescription>
+                Specify the size of each page. (WIP - only A4 right now)
+              </FormDescription>
+              <Select
+                onValueChange={field.onChange}
+                defaultValue={field.value}
+                disabled
+              >
+                <FormControl>
+                  <SelectTrigger className="w-32">
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="A4">A4</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+
+        {/* Submit Button */}
+        <Button
+          className="my-4"
+          type="submit"
+          disabled={!form.formState.isValid}
+        >
+          Generate QR Codes
+        </Button>
+      </form>
+      <div  ref={contentRef}>
+        <QrCodeHiddenPage file={file as File} bytesPerQrCode={256} />
+        <Button onClick={reactToPrintFn}>Print</Button>
+      </div>
+    </Form>
+  );
+}
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="mx-auto max-w-7xl p-8 flex justify-center items-center space-y-4">
+      <div className="mx-auto max-w-7xl p-8 space-y-4 flex flex-col justify-center items-center">
         <div className="flex h-5 items-center space-x-4 text-sm">
           <div>
             <P className="font-bold text-lg">Glacial</P>
@@ -16,9 +211,167 @@ function App() {
             <P>QR-based data archival and retrieval</P>
           </div>
         </div>
+        <Tabs defaultValue="archive" className="w-[520px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="archive">Archive</TabsTrigger>
+            <TabsTrigger value="retrieve">Retrieve</TabsTrigger>
+          </TabsList>
+          <TabsContent value="archive">
+            <Card>
+              <CardHeader>
+                <CardTitle>Archive</CardTitle>
+                <CardDescription>
+                  Encode your data as QR codes and print them out for long term
+                  offline storage.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <ArchiveForm />
+              </CardContent>
+              <CardFooter>{/* ??? */}</CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="retrieve">
+            <Card>
+              <CardHeader>
+                <CardTitle>Password</CardTitle>
+                <CardDescription>
+                  Change your password here. After saving, you'll be logged out.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="current">Current password</Label>
+                  <Input id="current" type="password" />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="new">New password</Label>
+                  <Input id="new" type="password" />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Save password</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
       </div>
     </ThemeProvider>
   );
 }
+
+interface QrCodeHiddenPage {
+  file: File;
+  bytesPerQrCode: number;
+}
+
+function QrCodeHiddenPage(props: QrCodeHiddenPage) {
+  const { file } = props;
+  const [z85Strings, setZ85Strings] = useState<string[]>();
+
+  useEffect(() => {
+    async function awaitArrayBuffer() {
+      const awaitedArrayBuffer = await file.arrayBuffer();
+      const int8Array = new Uint8Array(awaitedArrayBuffer);
+
+      const chunkedInt8Arrays = chunk(int8Array, 628);
+      const z85edArrayBuffers = chunkedInt8Arrays.map((buf) => encode(buf));
+      console.log(z85edArrayBuffers);
+      setZ85Strings(z85edArrayBuffers);
+    }
+    if (!z85Strings) {
+      awaitArrayBuffer();
+    }
+  }, [file, z85Strings]);
+
+  return (
+    <>
+      {
+        z85Strings && <StringTables strings={z85Strings} />
+        // z85Strings.map((z) => (
+        //     <QRCodeSVG  value={z} marginSize={4} />
+        // ))}
+      }
+    </>
+  );
+}
+
+function chunk(input: Uint8Array, sizePerChunk: number): Uint8Array[] {
+  // Check if the size is valid
+  if (sizePerChunk <= 0) {
+    throw new Error("Chunk size must be a positive integer");
+  }
+
+  const result: Uint8Array[] = [];
+  const totalChunks = Math.ceil(input.length / sizePerChunk);
+
+  for (let i = 0; i < totalChunks; i++) {
+    const start = i * sizePerChunk;
+    const end = Math.min(start + sizePerChunk, input.length);
+    const chunk = input.subarray(start, end);
+    result.push(chunk);
+  }
+
+  return result;
+}
+
+interface Props {
+  strings: string[];
+}
+
+const StringTables: React.FC<Props> = ({ strings }) => {
+  // Number of elements per table (2 columns x 3 rows)
+  const elementsPerTable = 6;
+
+  // Calculate the number of tables needed
+  const numTables = Math.ceil(strings.length / elementsPerTable);
+
+  return (
+    <div>
+      {Array.from({ length: numTables }, (_, tableIndex) => {
+        // Get the start and end index for slicing the strings array
+        const startIndex = tableIndex * elementsPerTable;
+        const endIndex = startIndex + elementsPerTable;
+        const currentTableStrings = strings.slice(startIndex, endIndex);
+
+        return (
+          <>
+            <div className="page-break" />
+
+            <P className="text-sm">
+              glacial | scan top to bottom, left to right | page{" "}
+              {tableIndex + 1}/{numTables}
+            </P>
+            <table key={tableIndex}>
+              <tbody>
+                {Array.from({ length: 3 }, (_, rowIndex) => (
+                  <tr key={rowIndex}>
+                    {/* Map over each column in the current row */}
+                    {Array.from({ length: 2 }, (_, colIndex) => {
+                      const stringIndex = rowIndex * 2 + colIndex;
+                      return (
+                        <td key={colIndex}>
+                          {stringIndex < currentTableStrings.length ? (
+                            <QRCodeSVG
+                              size={322}
+                              value={strings[stringIndex]}
+                              marginSize={2}
+                            />
+                          ) : (
+                            ""
+                          )}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        );
+      })}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/z85.ts
@@ -0,0 +1,131 @@
+/** 85 unique characters string */
+type CharSet = string;
+
+const ascii85: Uint8Array = charsetToMap(`!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_\`abcdefghijklmnopqrstu`);
+const z85:     Uint8Array = charsetToMap(`0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-:+=^!/*?&<>()[]{}@%$#`);
+
+const pow2 = 7225;     // 85 ** 2
+const pow3 = 614125;   // 85 ** 3
+const pow4 = 52200625; // 85 ** 4
+
+/** @param {"ascii85" | "z85" | string} [charset="z85"]
+ *  @return {Uint8Array}  */
+function getMap(charset: "ascii85" | "z85" | string = "z85"): Uint8Array {
+    if (charset === "ascii85") {
+        return ascii85;
+    }
+    if (charset.length === 85) {
+        return charsetToMap(charset);
+    }
+    return z85;
+}
+
+/** @param {string} charset - 85 characters ASCII string */
+function charsetToMap(charset: string):Uint8Array {
+    const ui8a = new Uint8Array(85);
+    for (let i = 0; i < 85; i++) {
+        ui8a[i] = charset.charAt(i).charCodeAt(0);
+    }
+    return ui8a;
+}
+
+/** @param {Uint8Array} mapOrig
+ *  @return {Uint8Array}  */
+function getReverseMap(mapOrig: Uint8Array): Uint8Array {
+    const revMap = new Uint8Array(128);
+    for (const [num, charCode] of Object.entries(mapOrig)) {
+        revMap[charCode] = parseInt(num);
+    }
+    return revMap;
+}
+
+/**
+ * Returns Base85 string.
+ * @param {Uint8Array} ui8a - input data to encode
+ * @param {"ascii85" | "z85" | string} [charset="z85"] - 85 unique characters string
+ * @return {string}
+ * */
+export function encode(ui8a: Uint8Array, charset?: "ascii85" | "z85" | CharSet): string {
+    const charMap = getMap(charset);
+    const remain = ui8a.length % 4;
+    const last5Length = remain ? remain + 1 : 0;
+    const length = Math.ceil(ui8a.length * 5 / 4);
+    const target = new Uint8Array(length);
+
+    const dw = new DataView(ui8a.buffer, ui8a.byteOffset, ui8a.byteLength);
+    const to = Math.trunc(ui8a.length / 4);
+    for (let i = 0; i < to; i++) {
+        let num = dw.getUint32(4 * i);
+        for (let k = 4; k >= 0; k--) {
+            target[k + i * 5] = charMap[num % 85];
+            num = Math.trunc(num / 85);
+        }
+    }
+
+    if (remain) {
+        const lastPartIndex = Math.trunc(ui8a.length / 4) * 4;
+        const lastPart = Uint8Array.from([...ui8a.slice(lastPartIndex), 0, 0, 0]);
+        const offset = target.length - last5Length - 1;
+        const dw = new DataView(lastPart.buffer);
+        let num = dw.getUint32(0);
+        for (let i = 4; i >= 0; i--) {
+            const value = charMap[num % 85];
+            num = Math.trunc(num / 85);
+            if (i < last5Length) {
+                const index = offset + i + 1;
+                target[index] = value;
+            }
+        }
+    }
+
+    return new TextDecoder().decode(target);
+}
+
+/**
+ * Decodes Base85 string.
+ * @param {string} base85 - base85 encoded string
+ * @param {"ascii85" | "z85" | string} [charset="z85"] - 85 unique characters string
+ * @return {Uint8Array}
+ * */
+export function decode(base85: string, charset?: "ascii85" | "z85" | CharSet): Uint8Array {
+    const map = getMap(charset);
+    const revMap = getReverseMap(map);
+
+    const base85ab = new TextEncoder().encode(base85);
+    const pad = (5 - (base85ab.length % 5)) % 5;
+
+    const ints = new Uint8Array((Math.ceil(base85ab.length / 5) * 4) - pad);
+    let dw = new DataView(ints.buffer);
+    let i = 0;
+    for (; i < base85ab.length / 5 - 1; i++) {
+        const c1 = revMap[base85ab[i * 5 + 4]];
+        const c2 = revMap[base85ab[i * 5 + 3]] * 85;
+        const c3 = revMap[base85ab[i * 5 + 2]] * pow2;
+        const c4 = revMap[base85ab[i * 5 + 1]] * pow3;
+        const c5 = revMap[base85ab[i * 5    ]] * pow4;
+        dw.setUint32(i * 4, c1 + c2 + c3 + c4 + c5);
+    }
+
+    const lCh = map[map.length - 1];
+    const lastPart = new Uint8Array([...base85ab.slice(i * 5), lCh, lCh, lCh, lCh]);
+    dw = new DataView(lastPart.buffer);
+    const c1 = revMap[lastPart[4]];
+    const c2 = revMap[lastPart[3]] * 85;
+    const c3 = revMap[lastPart[2]] * pow2;
+    const c4 = revMap[lastPart[1]] * pow3;
+    const c5 = revMap[lastPart[0]] * pow4;
+    dw.setUint32(0, c1 + c2 + c3 + c4 + c5);
+    for (let j = 0; j < 4 - pad; j++) {
+        ints[i * 4 + j] = lastPart[j];
+    }
+
+    return ints;
+}
+
+/** Alias to `encode`. */
+export const encodeBase85 = encode;
+/** Alias to `decode`. */
+export const decodeBase85 = decode;
+
+const base85 = {encode, decode, encodeBase85, decodeBase85};
+export default base85;
