e811f79583bbe3b740f96cf5513a84281188ab58
Ali <ali.adnani@hotmail.com>
2024-12-16 11:13:49 +0800

    fmt: format

--- src/features/archive/utils.ts
+++ src/features/archive/utils.ts
@@ -1,19 +1,19 @@
 function chunk(input: Uint8Array, sizePerChunk: number): Uint8Array[] {
-    if (sizePerChunk <= 0) {
-      throw new Error("Chunk size must be a positive integer");
-    }
-  
-    const result: Uint8Array[] = [];
-    const totalChunks = Math.ceil(input.length / sizePerChunk);
-  
-    for (let i = 0; i < totalChunks; i++) {
-      const start = i * sizePerChunk;
-      const end = Math.min(start + sizePerChunk, input.length);
-      const chunk = input.subarray(start, end);
-      result.push(chunk);
-    }
-  
-    return result;
+  if (sizePerChunk <= 0) {
+    throw new Error("Chunk size must be a positive integer");
   }
 
-  export { chunk }
+  const result: Uint8Array[] = [];
+  const totalChunks = Math.ceil(input.length / sizePerChunk);
+
+  for (let i = 0; i < totalChunks; i++) {
+    const start = i * sizePerChunk;
+    const end = Math.min(start + sizePerChunk, input.length);
+    const chunk = input.subarray(start, end);
+    result.push(chunk);
+  }
+
+  return result;
+}
+
+export { chunk };
